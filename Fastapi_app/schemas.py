00c4e1e8531a2bd21ba63e11c0b465418cbca177
Malak Ahmed Samih <164598112+Malak-A7med@users.noreply.github.com>
2025-11-28 01:36:08 +0200

    Add files via upload

---
+++ Fastapi_app/schemas.py
@@ -0,0 +1,9 @@
+# fastapi_app/schemas.py
+from pydantic import BaseModel
+from typing import List
+
+class PredictRequest(BaseModel):
+    features: List[float]
+
+class PredictResponse(BaseModel):
+    prediction: float
